b4f6c2b11ab03360a1b88c8810873ba9250349dc
cqoverly <cqoverly@gmail.com>
2013-11-02 11:10:00 -0700

    Updated urls.p for app_timeclock, app_manager, and root. These update include references to viewing timecard, viewing employees, editing timestamp, cand clocking in.

--- Timeclock/app_timeclock/urls.py
+++ Timeclock/app_timeclock/urls.py
@@ -5,8 +5,7 @@ import views
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'DjangoTest.views.home', name='home'),
-    # url(r'^DjangoTest/', include('DjangoTest.foo.urls')),
     url(r'^punch_clock$', views.vw_punch_clock, name='punch_clock'),
     url(r'^view_timecard$', views.vw_view_timecard, name='view_card'),
+    url(r'^edit_timestamp/(?P<pk>\d+)$', views.vw_edit_timestamp, name='edit_stamp'),
 )
