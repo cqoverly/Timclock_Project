c9d0f17af6289e64edc092a3cc711d0fd75b3c15
cqoverly <cqoverly@gmail.com>
2013-11-05 11:36:44 -0800

    Added admin for app_timeclock.models.TimestampEdits.

--- Timeclock/app_timeclock/admin.py
+++ Timeclock/app_timeclock/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from app_timeclock.models import Timestamp
+from app_timeclock.models import Timestamp, TimestampEdits
 
 
 class TimestampAdmin(admin.ModelAdmin):
@@ -8,4 +8,21 @@ class TimestampAdmin(admin.ModelAdmin):
     ordering = ['stamp']
 
 
+class TimestampEditsAdmin(admin.ModelAdmin):
+    list_display = (
+        'changed_by',
+        'for_employee',
+        'original_datetime',
+        'original_inout',
+        'new_datetime',
+        'new_inout',
+        'change_reason',
+        'date_changed',
+    )
+
+    list_filter = ['for_employee']
+    ordering = ['new_datetime']
+
+
 admin.site.register(Timestamp, TimestampAdmin)
+admin.site.register(TimestampEdits, TimestampEditsAdmin)
