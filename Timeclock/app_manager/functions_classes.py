e304f98c1dfc35a141d2b6329daee284d4ca7a22
cqoverly <cqoverly@gmail.com>
2013-11-07 09:10:08 -0800

    Created manager_only decorator

--- Timeclock/app_manager/functions_classes.py
+++ Timeclock/app_manager/functions_classes.py
@@ -1,3 +1,21 @@
-from django.contrib.auth.models import User
+# Python Std Library imports
+
+# Django imports
+from django.contrib.auth.models import User, Group
+from django.core.exceptions import PermissionDenied
+
+# Project imports
+
+
+def manager_only(func):
+    managers = User.objects.filter(groups__name='Manager')
+    def wrapper(*args, **kwargs):
+        request = args[0]
+        print request.user
+        if request.user in managers:
+            return func(*args, **kwargs)
+        else:
+            raise PermissionDenied
+    return wrapper
 
 
