e5d4440511596706fd9431b62bce01e23c522c31
cqoverly <cqoverly@gmail.com>
2013-11-07 13:14:13 -0800

    Added forms for viewing a range of timestamps, both Employee and Manager forms.

--- Timeclock/app_timeclock/forms.py
+++ Timeclock/app_timeclock/forms.py
@@ -1,13 +1,15 @@
+# Django framework imports
 from django import forms
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 
-from models import Timestamp
 import datetime
 
 # Project imports
-from .functions_classes import get_range, get_payperiod
+from .functions_classes import TimecardError, TimestampEntryError
+# from models import Timestamp
+# from .functions_classes import get_range, get_payperiod
 
-# formatting wisdget for Bootstrap
+# formatting widget for Bootstrap
 class_textwidget = forms.TextInput(attrs={'class': 'form-control'})
 
 class TimestampForm(forms.Form):
@@ -20,6 +22,11 @@ class TimestampForm(forms.Form):
     stamp = forms.DateTimeField(widget=class_textwidget)
     in_out = forms.ChoiceField(choices=CHOICES, widget=sel_widget)
 
+    # TODO: Define clean of TimestampForm to check for consistent in_out
+    def clean(self):
+        cleaned_data = super(TimestampForm, self).clean()
+        return cleaned_data
+
 
 class EditStampForm(forms.Form):
     class_selectwidget = forms.Select(attrs={'class': 'form-control'})
@@ -30,36 +37,38 @@ class EditStampForm(forms.Form):
     new_inout = forms.ChoiceField(choices=CHOICES, widget=sel_widget)
     change_reason = forms.CharField(max_length=255, widget=class_textareawidget)
 
-    # def clean(self):
-    #     cleaned_data = super(EditStampForm, self).clean()
-    #     original_stamp = cleaned_date('timestampID')
-    #     new_date = cleaned_data.get('new_datetime')
-    #     new_inout = cleaned_data.get('new_inout')
-    #     employee = original_stamp.employee
-    #     date_start, date_end = get_payperiod(new_date.date()).order_by('stamp')
-    #     # get a the payperiod into which we are trying to add updated stamp
-    #     check_range = get_range(employee, date_start, date_end)
-    #     # create a duplicate list of stamps in returned query result
-    #     test_newrange = check_range[:]
-    #     # find original
-    #     stamp_idx = test_newrange.index(original_stamp)
-
-
-
+    # TODO: Define clean of EditTimestampForm to check for consistent in_out
+    def clean(self):
+        cleaned_data = super(EditStampForm, self).clean()
+        return cleaned_data
 
 
 class EmpViewTimecardForm(forms.Form):
-    # name = forms.ModelChoiceField(queryset=Employee.objects.all())
     date = forms.DateTimeField(widget=class_textwidget)
 
 
 class ManagerViewTimecardForm(forms.Form):
     # get list of employees
     employees = User.objects.filter(groups__name='Employee')
-    # formatting widger for Bootstrap
+    # formatting widget for Bootstrap
     class_selectwidget = forms.Select(attrs={'class': 'form-control'})
     name = forms.ModelChoiceField(queryset=employees,
         widget=class_selectwidget)
     date = forms.DateTimeField(widget=class_textwidget)
 
+class EmpViewTimestampListForm(forms.Form):
+    start_date = forms.DateField(label="Start", widget=class_textwidget)
+    end_date = forms.DateField(label="End", widget=class_textwidget)
+
+class MgrViewTimestampListForm(forms.Form):
+    # get list of employees
+    emps = User.objects.filter(groups__name="Employee")
+    # custom widget needs to be reinitialized.
+    class_selectwidget = forms.Select(attrs={'class': 'form-control'})
+    employee = forms.ChoiceField(
+        widget=class_selectwidget,
+        queryset=emps,
+        label="Employee")
+    start_date = forms.DateField(label="Start")
+    end_date = forms.DateField(label="End")
 
