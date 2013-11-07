cc0a1b91d0014d733f5965f201f3e6270fba2cf1
cqoverly <cqoverly@gmail.com>
2013-11-07 13:50:12 -0800

    Added url for vw_list_timestamps.

--- Timeclock/app_timeclock/urls.py
+++ Timeclock/app_timeclock/urls.py
@@ -7,5 +7,10 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^punch_clock$', views.vw_punch_clock, name='punch_clock'),
     url(r'^view_timecard$', views.vw_view_timecard, name='view_card'),
-    url(r'^edit_timestamp/(?P<pk>\d+)$', views.vw_edit_timestamp, name='edit_stamp'),
+    url(r'^edit_timestamp/(?P<pk>\d+)$',
+        views.vw_edit_timestamp,
+        name='edit_stamp'),
+    url(r'^list_timestamps$',
+        views.vw_list_timestamps,
+        name='list_stamps'),
 )
