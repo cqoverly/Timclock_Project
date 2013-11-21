c86c24e8e08855717883e75e8ee98331f138d2df
cqoverly <cqoverly@gmail.com>
2013-11-20 16:37:23 -0800

    Added custom form class with custom clean to check for in_out stamp consistency and raise error if needed. Changed TimestampForm and Edit TimestampForm to subclass from new class(StampMixin)

--- Timeclock/app_timeclock/forms.py
+++ Timeclock/app_timeclock/forms.py
@@ -6,13 +6,43 @@ import datetime
 
 # Project imports
 from .functions_classes import TimecardError, TimestampEntryError
+from .models import Timestamp
 # from models import Timestamp
 # from .functions_classes import get_range, get_payperiod
 
 # formatting widget for Bootstrap
 class_textwidget = forms.TextInput(attrs={'class': 'form-control'})
 
-class TimestampForm(forms.Form):
+
+class StampMixin(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        try:
+            self.orig_stamp = kwargs.pop('orig_stamp')
+        except KeyError:
+            pass
+        super(StampMixin, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = super(StampMixin, self).clean()
+        # get the previous stamp
+        print self.__dict__
+        last = Timestamp.objects.filter(
+            user=self.orig_stamp.user,
+            stamp__lt=Timestamp.objects.get(pk=self.orig_stamp.pk).stamp)
+        last = last.latest('stamp')
+
+        l_stamp = last.stamp
+        print "{} -- {}".format(last.in_out, l_stamp)
+        print self.cleaned_data['new_inout']
+        if last.in_out == self.cleaned_data.get('new_inout'):
+            print 'RAISING ERROR'
+            raise TimestampEntryError('IN/OUT not in correct order.')
+
+        return cleaned_data
+
+
+class TimestampForm(StampMixin):
     """
     Form for generating new timestamp.
     """
@@ -22,13 +52,8 @@ class TimestampForm(forms.Form):
     stamp = forms.DateTimeField(widget=class_textwidget)
     in_out = forms.ChoiceField(choices=CHOICES, widget=sel_widget)
 
-    # TODO: Define clean of TimestampForm to check for consistent in_out
-    def clean(self):
-        cleaned_data = super(TimestampForm, self).clean()
-        return cleaned_data
 
-
-class EditStampForm(forms.Form):
+class EditStampForm(StampMixin):
     class_selectwidget = forms.Select(attrs={'class': 'form-control'})
     class_textareawidget = forms.Textarea(attrs={'class': 'form-control'})
     sel_widget = class_selectwidget
@@ -36,11 +61,6 @@ class EditStampForm(forms.Form):
     CHOICES = (('IN', 'IN'), ('OUT', 'OUT'))
     new_inout = forms.ChoiceField(choices=CHOICES, widget=sel_widget)
     change_reason = forms.CharField(max_length=255, widget=class_textareawidget)
-
-    # TODO: Define clean of EditTimestampForm to check for consistent in_out
-    def clean(self):
-        cleaned_data = super(EditStampForm, self).clean()
-        return cleaned_data
 
 
 class EmpViewTimecardForm(forms.Form):
