e49ec26f0d9160481e839ba78ed043bc8761b2a3
Chris Overly <cqoverly@gmail.com>
2013-11-27 16:17:14 -0800

    Added url for vw_edit_employee

--- Timeclock/app_manager/urls.py
+++ Timeclock/app_manager/urls.py
@@ -5,12 +5,7 @@ import views
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', views.vw_home, name='home'),
-    # url(r'^view-employees$', views.vw_view_employees, name='view_emps'),
-    # url(r'^view-employees$', views.EmployeeList.as_view(), name='view_emps'),
-    # url(r'^edit_employee/(?P<pk>\d+)/$', views.vw_edit_employee, name='edit_emp'),
-    # url(r'^employee_detail/(?P<username>\w+)/$',
-    #     views.vw_employee_detail,
-    #     name='emp_detail'),
+    url(r'^edit_employee/(?P<pk>\d+)/$', views.vw_edit_employee, name='edit_emp'),
     url(r'^add_employee$', views.vw_add_employee, name='add_emp'),
     url(r'^change_password$', views.vw_change_password, name='change_password'),
     url(r'^view_employees$', views.EmployeeList.as_view(), name='view_emps'),
